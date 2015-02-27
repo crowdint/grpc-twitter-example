8f44a6ae17aee1d9afce0c16a12c495c50015309
Jacques Fuentes <jpfuentes2@gmail.com>
2015-02-27 17:38:16 -0500

    Add comment explaining conn pooling

--- web.go
+++ web.go
@@ -13,6 +13,7 @@ var user = &pb.User{ID: 1}
 func main() {
 	router := gin.Default()
 	router.GET("/tweets/:user_id", func(c *gin.Context) {
+		// TODO(jpfuentes2): connection pooling or reuse
 		conn, _ := grpc.Dial("127.0.0.1:10000")
 		defer conn.Close()
 
